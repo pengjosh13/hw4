7bdfb71d5d8be4a146c990a0fc8ac667c203df39
Joshua Heesik Kim <jkim5016@usc.edu>
2022-10-26 21:01:46 +0000

    equal paths done

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,12 +1,35 @@
 #include "equal-paths.h"
+
 using namespace std;
 
 
 // You may add any prototypes of helper functions here
 
 
-bool equalPaths(Node * root)
+bool checkPaths(Node *root, int lvl, int *leafLvl){
+	if (root == nullptr){
+		return true;
+	}
+
+	if (root->left == nullptr && root->right == nullptr){
+		if (*leafLvl == 0){
+			*leafLvl = lvl;
+			return true;
+		}
+		return (lvl == *leafLvl);
+
+	}
+	return (checkPaths(root->left, lvl + 1, leafLvl) && checkPaths(root->right, lvl + 1, leafLvl));
+
+}
+
+bool equalPaths(Node *root)
 {
+	int lvl = 0;
+	int leafLvl = 0;
+	return checkPaths(root, lvl, &leafLvl);
+
+	
     // Add your code below
 
 }
